4edfeb79a47c944ffe5fbc677e9912124d1329dc
sam-part <samparting06@gmail.com>
2023-07-19 18:22:24 -0400

    idk how git works

--- platformer/main.cpp
+++ platformer/main.cpp
@@ -9,26 +9,14 @@ int main(int argc, char* args[])
 {
 	Window window(800, 600, "Game");
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	std::vector<std::unique_ptr<GameObject>> game_objects;
+
+	game_objects.push_back(std::make_unique<Block>(&window, 100, 100));
+
+	while (window.IsOpen())
 	{
-		std::cout << "Could not initialize SDL. SDL_Error: " << SDL_GetError() << "\n";
-		return 0;
+		window.PollEvents();
+		window.Clear();
+		window.Refresh();
 	}
-
-	window = SDL_CreateWindow("Platformer Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-	if (window == NULL)
-	{
-		std::cout << "Could not create window. SDL_Error: " << SDL_GetError() << "\n";
-		return 0;
-	}
-
-	surface = SDL_GetWindowSurface(window);
-
-	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 255, 0, 0));
-
-	//Update the surface
-	SDL_UpdateWindowSurface(window);
-
-	//Hack to get window to stay up
-	SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
 }
