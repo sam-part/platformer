7f2925fe8b96fba6d0ce0315a5266e7b407a3335
sweavertheslayr <sweavertheslayr@outlook.com>
2023-07-20 18:00:25 -0400

    grrgrg

--- platformer/player.cpp
+++ platformer/player.cpp
@@ -7,16 +7,27 @@ Player::Player(Window* window, SDL_Texture* texture, double x, double y, double 
 {
 	position.x = x;
 	position.y = y;
-	dimensions.width = width;
-	dimensions.height = height;
+	dimensions.x = width;
+	dimensions.y = height;
+
+	destination.x = position.x;
+	destination.y = position.y;
+	destination.w = dimensions.x;
+	destination.h = dimensions.y;
+
 
 	// calculates dimensions of the players texture
 	SDL_Point texture_dimensions{0, 0};
 	SDL_QueryTexture(texture, NULL, NULL, &texture_dimensions.x, &texture_dimensions.y);
 
+	source.x = position.x;
+	source.y = 0;
+	source.w = texture_dimensions.x;
+	source.h = texture_dimensions.y;
+
 	animation_data = {
 		{double(texture_dimensions.x), double(texture_dimensions.y)},
-		{dimensions.width, dimensions.height},
+		{dimensions.x, dimensions.y},
 		0,
 		0,
 		1.0 / 6.0
@@ -32,8 +43,8 @@ SDL_Rect Player::Destination()
 	SDL_Rect destination{
 		position.x,
 		position.y,
-		dimensions.width,
-		dimensions.height
+		dimensions.x,
+		dimensions.y
 	};
 
 	return destination;
@@ -69,6 +80,9 @@ void Player::Update(double dt)
 		Move(Directions::Right, base_speed);
 	}
 
+	destination.x = position.x;
+	destination.y = position.y;
+
 	// animate
 	animation_data.running_time += dt;
 	if (animation_data.running_time >= animation_data.update_time)
@@ -81,11 +95,12 @@ void Player::Update(double dt)
 			animation_data.frame = 0;
 		}
 	}
+
+	source.x = 0 + animation_data.player_dimensions.width * animation_data.frame;
+	source.y = position.y;
 }
 
 void Player::Draw()
 {
-	SDL_Rect source = Source();
-	SDL_Rect destination = Destination();
 	window->Draw(texture, source, destination);
 }
